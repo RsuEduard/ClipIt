4861d65e4998708aa242996d0355b458ed938a10
RsuEduard <rusu00eduard@proton.me>
2023-12-07 18:45:32 +0200

    feat: created components for reusable tabs

--- clipz/src/app/shared/modal/modal.component.ts
+++ clipz/src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,10 +6,13 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() id: string = '';
 
-  constructor(public modal: ModalService) {
+  constructor(public modal: ModalService, public el: ElementRef) {}
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
   toggleModal(): void {
