6f337f3c3b8b9c1602ae99388ffb2d3eb4fd34f4
RsuEduard <rusu00eduard@proton.me>
2023-12-07 19:19:23 +0200

    feat: working tabs components

--- clipz/src/app/shared/tabs-container/tabs-container.component.ts
+++ clipz/src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  OnInit,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.scss']
+  styleUrls: ['./tabs-container.component.scss'],
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
+  ngAfterContentInit(): void {
+    const activeTab = this.tabs.find((tab) => tab.active);
+    if (!activeTab) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent): boolean {
+    this.tabs.forEach((tab) => {
+      tab.active = false;
+    });
+
+    tab.active = true;
+
+    //prevent default behaviour
+    return false;
+  }
+
+  getTabClass(tab: TabComponent): string {
+    if (tab.active) {
+      return 'hover:text-white text-white bg-indigo-400';
+    }
+    return 'hover:text-indigo-400';
+  }
 }
