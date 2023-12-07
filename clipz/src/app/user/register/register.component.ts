129387068d3fb87431a686a17fc83d3c9cd36e20
RsuEduard <rusu00eduard@proton.me>
2023-12-07 19:39:46 +0200

    feat: created form group for registration form

--- clipz/src/app/user/register/register.component.ts
+++ clipz/src/app/user/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
+  registerFrom = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
 }
