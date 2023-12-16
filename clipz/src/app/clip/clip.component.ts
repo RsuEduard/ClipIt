45b5b1cf5f1aa1f5f39bda054d00cca895294f38
RsuEduard <rusu00eduard@proton.me>
2023-12-16 10:56:46 +0200

    feat: persisted selection from dropdown inside manage page

--- clipz/src/app/clip/clip.component.ts
+++ clipz/src/app/clip/clip.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.scss']
+  styleUrls: ['./clip.component.scss'],
 })
-export class ClipComponent {
+export class ClipComponent implements OnInit {
+  id: string = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+    });
+  }
 }
