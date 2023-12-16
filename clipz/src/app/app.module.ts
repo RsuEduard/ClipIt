81019b8856922a48556cc4acfc8cee8b29fa74da
RsuEduard <rusu00eduard@proton.me>
2023-12-16 11:04:34 +0200

    feat: created page not found component

--- clipz/src/app/app.module.ts
+++ clipz/src/app/app.module.ts
@@ -13,16 +13,24 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent, ClipComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ClipComponent,
+    NotFoundComponent,
+  ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     UserModule,
     BrowserModule,
-    AppRoutingModule,
     AngularFireAuthModule,
     AngularFirestoreModule,
     VideoModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
