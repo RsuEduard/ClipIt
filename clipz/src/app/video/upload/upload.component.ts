e2a3040ad823effbf832e38c4923c6d2223dbf30
RsuEduard <rusu00eduard@proton.me>
2023-12-20 13:38:39 +0200

    feat: redirect the user to the clip page after successful video upload

--- clipz/src/app/video/upload/upload.component.ts
+++ clipz/src/app/video/upload/upload.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import {
   AngularFireStorage,
   AngularFireStorageReference,
+  AngularFireUploadTask,
 } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { v4 as uuid } from 'uuid';
@@ -10,13 +11,14 @@ import firebase from 'firebase/compat/app';
 import { last, switchMap } from 'rxjs';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss'],
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
   user: firebase.User | null = null;
 
   isDragover: boolean = false;
@@ -39,14 +41,21 @@ export class UploadComponent {
     title: this.title,
   });
 
+  private task: AngularFireUploadTask | null = null;
+
   constructor(
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private router: Router
   ) {
     this.auth.user.subscribe((user) => {
       this.user = user;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.task?.cancel();
   }
 
   storeFile($event: Event) {
@@ -76,21 +85,21 @@ export class UploadComponent {
     this.clipFileName = uuid();
     const clipPath = `clips/${this.clipFileName}.mp4`;
 
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
     const clipRef = this.storage.ref(clipPath);
 
-    task.percentageChanges().subscribe((progress) => {
+    this.task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
 
-    task
+    this.task
       .snapshotChanges()
       .pipe(
         last(),
         switchMap(() => clipRef.getDownloadURL())
       )
       .subscribe({
-        next: (url) => {
+        next: async (url) => {
           const clip: IClip = {
             uid: this.user?.uid!,
             displayName: this.user?.displayName!,
@@ -99,11 +108,15 @@ export class UploadComponent {
             url,
           };
 
-          this.clipService.createClip(clip);
+          const clipDocRef = await this.clipService.createClip(clip);
 
           this.alertMsg = 'File uploaded successfully';
           this.alertColor = 'green';
           this.isPercentageVisible = false;
+
+          setTimeout(() => {
+            this.router.navigate(['clip', clipDocRef.id]);
+          }, 1000);
         },
         error: (error) => {
           this.formGroup.enable();
