7afb50ecfb2a94cd6a8c8a1e4b0ef315de890aab
RsuEduard <rusu00eduard@proton.me>
2023-12-20 11:58:03 +0200

    feat: save user details when uploading a file

--- clipz/src/app/video/upload/upload.component.ts
+++ clipz/src/app/video/upload/upload.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFireStorage,
+  AngularFireStorageReference,
+} from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { v4 as uuid } from 'uuid';
+import firebase from 'firebase/compat/app';
+import { last, switchMap } from 'rxjs';
+import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clip.model';
 
 @Component({
   selector: 'app-upload',
@@ -9,11 +17,14 @@ import { v4 as uuid } from 'uuid';
   styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
-  constructor(private storage: AngularFireStorage) {}
+  user: firebase.User | null = null;
 
   isDragover: boolean = false;
   file: File | null = null;
   isFormVisible: boolean = false;
+  percentage: number = 0;
+  isPercentageVisible: boolean = false;
+  clipFileName: string = '';
 
   isAlertVisible: boolean = false;
   alertColor: string = 'blue';
@@ -27,6 +38,16 @@ export class UploadComponent {
   formGroup = new FormGroup({
     title: this.title,
   });
+
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipService: ClipService
+  ) {
+    this.auth.user.subscribe((user) => {
+      this.user = user;
+    });
+  }
 
   storeFile($event: DragEvent) {
     this.isDragover = false;
@@ -45,15 +66,46 @@ export class UploadComponent {
     this.isAlertVisible = true;
     this.alertColor = 'blue';
     this.alertMsg = 'Uploading file...';
-    
-    const clipFileName = uuid();
-    const clipPath = `clips/${clipFileName}.mp4`;
+    this.isPercentageVisible = true;
+
+    this.clipFileName = uuid();
+    const clipPath = `clips/${this.clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
-    task.then((_) => {
-      this.isAlertVisible = true;
-      this.alertMsg = 'File uploaded successfully';
-      this.alertColor = 'green';
+    const clipRef = this.storage.ref(clipPath);
+
+    task.percentageChanges().subscribe((progress) => {
+      this.percentage = (progress as number) / 100;
     });
+
+    task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
+      .subscribe({
+        next: (url) => {
+          const clip: IClip = {
+            uid: this.user?.uid!,
+            displayName: this.user?.displayName!,
+            title: this.title.value,
+            fileName: `${this.clipFileName}.mp4`,
+            url,
+          };
+
+          this.clipService.createClip(clip);
+
+          this.alertMsg = 'File uploaded successfully';
+          this.alertColor = 'green';
+          this.isPercentageVisible = false;
+        },
+        error: (error) => {
+          this.alertMsg = 'File upload failed';
+          this.alertColor = 'red';
+          this.inSubmission = false;
+          this.isPercentageVisible = false;
+        },
+      });
   }
 }
